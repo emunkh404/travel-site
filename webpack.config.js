3910e3ed8f4dcf283769baf9cd239c0f554abff4
Munkh Erdene <m.erdene404@gmail.com>
2023-10-06 12:48:23 -0400

    large-hero, btn, variables, minins added

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,74 @@
 const path = require("path");
-const postCSSPlugins = [require('postcss-simple-vars'), require('postcss-nested'), require("autoprefixer")]
+const postCSSPlugins = [
+    require("postcss-import"), 
+    require('postcss-simple-vars'), 
+    require('postcss-nested'), 
+    require("autoprefixer"),
+    require("postcss-mixins")
+];
 
 module.exports = {
-    entry: "./app/assets/scripts/App.js",
-    output: {
-        filename: "bundled.js",
-        path: path.resolve(__dirname, "app")
-    },
-    mode: "development",
-    watch: true,
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader','css-loader', {
-                    loader: 'postcss-loader', options: {
-                        postcssOptions: {plugins: postCSSPlugins}
-                    }
-                }]
-            }
-        ]
-    }
-}
+  entry: "./app/assets/scripts/App.js",
+  output: {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app"),
+  },
+  devServer: {
+    static: path.join(__dirname, "app"),
+    hot: true,
+    port: 3000,
+    watchFiles: ["./app/**/*.html"],
+    // onBeforeSetupMiddleware: function(app, server) {
+    //     server._watch("./app/**/*.html");
+    //   },
+    host: '0.0.0.0'
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", {
+          loader: "postcss-loader",
+          options: {
+            postcssOptions: { plugins: postCSSPlugins },
+          },
+        }],
+      },
+    ],
+  },
+};
+
+// const path = require("path");
+// const postCSSPlugins = [require("postcss-import"), require('postcss-simple-vars'), require('postcss-nested'), require("autoprefixer")]
+
+// module.exports = {
+//     entry: "./app/assets/scripts/App.js",
+//     output: {
+//         filename: "bundled.js",
+//         path: path.resolve(__dirname, "app")
+//     },
+//     devServer: {
+//         static: {
+//             directory: path.join(__dirname, "app"), // Specify the content directory
+//         },
+//         hot: true,
+//         port: 3000,
+//         onBeforeSetupMiddleware: function (devServer) {
+//             devServer._watch("./app/**/*.html");
+//         }
+//     },
+//     mode: "development",  
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.css$/i,
+//                 use: ['style-loader','css-loader', {
+//                     loader: 'postcss-loader', options: {
+//                         postcssOptions: {plugins: postCSSPlugins}
+//                     }
+//                 }]
+//             }
+//         ]
+//     }
+// }
